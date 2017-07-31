c6cc87e8e8ca3b99967ed706ead788ccc1a9a1f9
yangzuo <zyang0508@163.com>
2017-07-31 20:35:26 +0800

    1 tow sum

---
+++ 1.TwoSum/Solution.java
@@ -0,0 +1,25 @@
+package com.test.spring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 解题思路：
+ * 使用一个Map，保存数组元素及其索引的键值对。
+ */
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>(len);
+        for (int i = 0; i < len; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] {map.get(diff), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
