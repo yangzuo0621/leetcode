746a3f8787263d7d22689061ba5c85b33b2e6ae5
yangzuo <zyang0508@163.com>
2017-08-10 23:03:58 +0800

    217. Contains Duplicate

---
+++ 217.ContainsDuplicate/Solution.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 解题思路：
+ */
+public class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i]))
+                return true;
+            else
+                set.add(nums[i]);
+        }
+        return false;
+    }
+}
