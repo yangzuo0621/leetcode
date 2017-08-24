0b796a2736c32887bdb67fb97428d68037d0981c
bjyangzuo <bjyangzuo@corp.netease.com>
2017-08-24 10:54:40 +0800

    2. Add Two Numbers

--- 002.AddTwoNumbers/Solution.java
+++ 002.AddTwoNumbers/Solution.java
@@ -11,7 +11,6 @@
  * }
  */
 public class Solution {
-
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode node = dummyHead, p = l1, q = l2;
